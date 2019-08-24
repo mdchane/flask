b386eaf57019868b6e816bb2f7087672890519db
mdchane <mohamed.dchane@outlook.fr>
2019-08-24 20:32:03 +0200

    decorator

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 # object Flask from flask package
+from config import Config
+
 app = Flask(__name__)
 # var app = instance of class Flask : Py predefined var to configure flask correctly
+app.config.from_object(Config)
+
 from app import routes
 # app here is the package 
 # routes module : URLs
