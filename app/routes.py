22f4da043a885a69ec4cb2ee65e2543b81c88b0f
mdchane <mohamed.dchane@outlook.fr>
2019-08-24 19:43:57 +0200

    chap 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 # package / flask instance
+
 @app.route('/')
 @app.route('/index')
 # decorators : associate URL and arg given
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mohamed'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    # list of dictio with author and body fields
+    return render_template('index.html', title='Home', user=user, posts=posts)
+# fct convert temp to HTML
